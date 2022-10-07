d47ce6f0dc90584d89f449345c2cb098824088f8
farhannovaldi <112691854+farhannovaldi@users.noreply.github.com>
2022-10-07 08:22:01 +0700

    Add files via upload

---
+++ jobsheet8/src/js8/ArraySample2.java
@@ -0,0 +1,18 @@
+package js8;
+
+/*
+ * Farhan Novaldi
+ * 21343024
+ * Informatika
+ */
+/**
+ * @author Farhan_Nvl
+ */
+public class ArraySample2 {
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i]);
+        }
+    }
+}
---
+++ jobsheet8/src/js8/ArraySample3.java
@@ -0,0 +1,20 @@
+package js8;
+
+/*
+ * Farhan Novaldi
+ * 21343024
+ * Informatika
+ */
+/**
+ * @author Farhan_Nvl
+ */
+public class ArraySample3 {
+    public static void main(String[] args) {
+        String[][] dogs = { { "Terry", "brown" },
+                { "Kristin", "white" },
+                { "Toby", "gray" },
+                { "Fido", "black" }
+        };
+        System.out.println(dogs[0][0]);
+    }
+}
